df887c5a7d646e4d82dc7d4eb6f05f1977ea76cd
Nazar <stanchenkon@gmail.com>
2021-11-25 01:48:12 +0200

    finish lesson07

--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -4,7 +4,7 @@ import org.testng.annotations.*;
 
 public class TestCalculator
 {
-    private Calculator calculator = new Calculator();
+    public Calculator calculator = new Calculator();
 
     @BeforeSuite
     public void beforeSuit() //будит запускаться перед каждым методом тестового класса
@@ -35,6 +35,23 @@ public class TestCalculator
         System.out.println("BeforeMethod");
     }
 
+    @AfterTest
+    public void afterTest(){System.out.println("AfterTest");}
+
+    @AfterMethod
+    public void afterMethod(){System.out.println("AfterMethod");}
+
+    @AfterGroups
+    public void afterGroup(){System.out.println("AfterGroup");}
+
+    @AfterClass
+    public void afterClass(){System.out.println("AfterClass");}
+
+    @AfterSuite
+    public void afterSuit(){System.out.println("AfterSuit");}
+
+
+    //при наличии одинакового приоритета тесты будут выполнятся в порядке их написания по строкам кода
     //@Test(enabled = false, description = "девы поломали тест") //отключение теста
     @Test(priority = 1)  // приоритетность выполнения тестов
     public void testSum1()
@@ -43,15 +60,20 @@ public class TestCalculator
         //Assert.assertEquals();
         //Assert.assertFalse();
 
-        Assert.assertEquals(calculator.sum(4,6), 10);
+        Assert.assertEquals(calculator.sum(7,3), 10);
+        System.out.println("testSum1: 7 и 3 = "+calculator.sum(7,3));
     }
-    @Test  //анотация default prioriti = 0 и он будит выполняться первым
-    public void testSum2() {Assert.assertEquals(calculator.sum(4,6), 10);}
+    @Test(priority = 1)  //анотация default prioriti = 0 и он будит выполняться первым
+    public void testSum2()
+    {Assert.assertEquals(calculator.sum(4,6), 10);
+        System.out.println("testSum2: 4 и 6 = "+calculator.sum(4,6));
+    }
 
     @Test(timeOut = 3000) // таймаут 3 сек
     public void testSum3()
     {
-        Assert.assertEquals(calculator.sum(4,6), 10);
+        Assert.assertEquals(calculator.sum(5,15), 20);
+        System.out.println("Sum = "+calculator.sum(5,15));
     }
 
     @Test(dependsOnMethods = "testSum1") //выполнение 4 теста будит зависить от того выполнится ли основной тест
