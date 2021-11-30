3de8f54c25562f170199e9257b2a117e485c9121
Nazar <stanchenkon@gmail.com>
2021-11-30 09:37:03 +0200

    finish lesson09

---
+++ main/lesson09/listing1/SeleniumStart.java
@@ -0,0 +1,19 @@
+package lesson09.listing1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumStart
+{
+    public static void main(String[] args) {
+      //  System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+        WebDriverManager.chromedriver().setup(); // при таком сетапе нет необходимости следить за версией хромдрайвера
+       // WebDriverManager.safaridriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demoqa.com");
+        //driver.close(); //закрывает одну вкладку
+       driver.quit();  // закрывает окно открытое драйвером в не зависимости от количества  вкладок
+        
+    }
+}
